e876f0d8c089c09060fa333bcea9d113c8a1531b
Florian Ranze <florian.ranze@gmail.com>
2023-04-01 23:06:34 +0200

    dotenv condition for windows added

--- src/constants/dotenv.ts
+++ src/constants/dotenv.ts
@@ -1,11 +1,11 @@
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
 
-import path from "path";
-// import { fileURLToPath } from "url";
+if (process.env.OS === "Windows_NT") {
+  const local = process.env.npm_config_local_prefix;
+  if (local === undefined) {
+    throw new Error("npm_config_local_prefix from process.env is undefined");
+  }
+  dotenv.config({ path: local + "\\src\\.env" });
+}
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// dotenv.config({ path: __dirname + "/../../.env" });
-dotenv.config()
-
+dotenv.config();
