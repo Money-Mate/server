ce19fa9c23e5f0a4e8396500a59760a7b4342adf
Florian Ranze <florian.ranze@gmail.com>
2023-04-03 09:37:58 +0200

    changed location of .env

--- src/constants/dotenv.ts
+++ src/constants/dotenv.ts
@@ -1,11 +1,3 @@
 import * as dotenv from "dotenv";
 
-if (process.env.OS === "Windows_NT") {
-  const local = process.env.npm_config_local_prefix;
-  if (local === undefined) {
-    throw new Error("npm_config_local_prefix from process.env is undefined");
-  }
-  dotenv.config({ path: local + "\\src\\.env" });
-}
-
 dotenv.config();
