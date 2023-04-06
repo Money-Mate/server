1fd47f2bacaa6bee812ac44f4e0abc414239f504
Lorp91 <92474455+Lorp91@users.noreply.github.com>
2023-04-06 02:00:07 +0200

    renamed balance to bankBalance

--- src/models/DashboardData.ts
+++ src/models/DashboardData.ts
@@ -2,7 +2,7 @@ import mongoose, { Schema, Model, Types } from "mongoose";
 
 export interface IDashboardData {
   user: Types.ObjectId;
-  balance: number;
+  bankBalance: number;
   saved: number;
   scheduledDebit: number;
   balanceEndOfMonth: number;
@@ -12,7 +12,7 @@ type DashboardDataModel = Model<IDashboardData>;
 
 const dashboardDataSchema = new Schema<IDashboardData, DashboardDataModel>({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  balance: { type: Number, default: 0 },
+  bankBalance: { type: Number, default: 0 },
   saved: { type: Number, default: 0 },
   scheduledDebit: { type: Number, default: 0 },
   balanceEndOfMonth: { type: Number, default: 0 },
