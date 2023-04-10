7622725d0fda13ae075ae407f8c767aa3877686b
Lorp91 <92474455+Lorp91@users.noreply.github.com>
2023-04-10 17:54:37 +0200

    nameAliases added

--- src/models/Recipient.ts
+++ src/models/Recipient.ts
@@ -3,6 +3,7 @@ import mongoose, { Schema, Model, Types } from "mongoose";
 interface IRecipient {
   user: Types.ObjectId;
   name: string;
+  nameAliases?: string[];
   usedInTransactions?: Types.ObjectId[];
   usedIBANs?: string[];
 }
@@ -12,6 +13,7 @@ type RecipientModel = Model<IRecipient>;
 const recipientSchema = new Schema<IRecipient, RecipientModel>({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   name: { type: String, required: true },
+  nameAliases: { type: [String] },
   usedInTransactions: {
     type: [
       {
@@ -20,7 +22,7 @@ const recipientSchema = new Schema<IRecipient, RecipientModel>({
       },
     ],
   },
-  usedIBANs: { type: Array },
+  usedIBANs: { type: [String] },
 });
 
 const Recipient = mongoose.model<IRecipient, RecipientModel>(
