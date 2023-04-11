99ee0dfef547ffd65390f1979eda7454dbc36090
Lorp91 <92474455+Lorp91@users.noreply.github.com>
2023-04-11 14:29:05 +0200

    writeDashboard attached to request

--- src/controllers/dashboardController.ts
+++ src/controllers/dashboardController.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import DashboardData from "../models/DashboardData";
+import { writeDashboardData } from "../utils/dashboarddata-generator";
 
 export const getDashboardData = async (req: Request, res: Response) => {
   try {
+    await writeDashboardData(res.locals.user._id);
     const data = await DashboardData.findOne({
       user: res.locals.user._id,
     }).select({ _id: false, user: false });
