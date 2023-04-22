2d679911db8b6f4b2ab8342d4f06ad733f5e0272
Lorp91 <92474455+Lorp91@users.noreply.github.com>
2023-04-22 07:21:07 +0200

    subCategory validation added

--- src/routes/subCategoryRoute.ts
+++ src/routes/subCategoryRoute.ts
@@ -1,17 +1,77 @@
 import express from "express";
 import * as controller from "../controllers/subCategoryController";
 import authenticate from "../middlewares/authenticate";
+import z from "zod";
+import { Types } from "mongoose";
+import { validate } from "../middlewares/validation";
 
 const router = express.Router();
 
-router.post("/add", authenticate, controller.addSubCategory);
+const addSchema = z.object({
+  body: z.object({
+    name: z.string({
+      invalid_type_error: "name needs to be a string",
+      required_error: "name is required",
+    }),
+    parentCategory: z
+      .string({
+        invalid_type_error: "parentCategory needs to be a string",
+        required_error: "parentCategory is required",
+      })
+      .refine((id: string) => Types.ObjectId.isValid(id), {
+        message: "invalid ObjectId",
+      }),
+  }),
+});
+
+const updateSchema = z.object({
+  body: z.object({
+    subCategoryId: z
+      .string({
+        invalid_type_error: "subCategoryId needs to be a string",
+        required_error: "subCategoryId is required",
+      })
+      .refine((id: string) => Types.ObjectId.isValid(id), {
+        message: "invalid ObjectId",
+      }),
+    data: z.object({
+      name: z
+        .string({
+          invalid_type_error: "name needs to be a string",
+          required_error: "name is required",
+        })
+        .optional(),
+      parentCategory: z
+        .string({
+          invalid_type_error: "parentCategory needs to be a string",
+          required_error: "parentCategory is required",
+        })
+        .refine((id: string) => Types.ObjectId.isValid(id), {
+          message: "invalid ObjectId",
+        })
+        .optional(),
+    }),
+  }),
+});
+
+router.post(
+  "/add",
+  authenticate,
+  validate(addSchema),
+  controller.addSubCategory
+);
 router.get("/getAllMy", authenticate, controller.getMySubCategorys);
 router.get(
   "/getSubByCategory/:id",
   authenticate,
   controller.getMySubCategoriesByCategory
 );
-router.put("/updateMy", authenticate, controller.updateMySubCategory);
+router.put(
+  "/updateMy",
+  authenticate,
+  validate(updateSchema),
+  controller.updateMySubCategory
+);
 router.delete(
   "/deleteMy/:id",
   authenticate,
