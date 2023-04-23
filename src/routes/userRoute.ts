b2abe90735ec7a7216fffa0a7fe07eb2851928f9
Lorp91 <92474455+Lorp91@users.noreply.github.com>
2023-04-23 21:23:59 +0200

    user validation added

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,13 +1,22 @@
 import express from "express";
 import * as controller from "../controllers/userController";
 import authenticate from "../middlewares/authenticate";
-import validateUser from "../middlewares/userValidation";
 import isAdmin from "../middlewares/isAdmin";
+import z from "zod";
+import { validate } from "../middlewares/validation";
 
 const router = express.Router();
 
+const addSchema = z.object({
+  body: z.object({
+    username: z.string().trim().min(3),
+    email: z.string().email(),
+    password: z.string(),
+  }),
+});
+
 router.post("/login", controller.login);
-router.post("/register", validateUser, controller.register);
+router.post("/register", validate(addSchema), controller.register);
 router.get("/logout", controller.logout);
 router.get("/checkToken", authenticate, controller.checkToken);
 
