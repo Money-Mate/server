d490f52d4bd42f54e4da01ed25a49f96c4d4922f
Lorp91 <92474455+Lorp91@users.noreply.github.com>
2023-04-23 20:57:47 +0200

    category validation added

--- src/routes/categoryRoute.ts
+++ src/routes/categoryRoute.ts
@@ -1,12 +1,33 @@
 import express from "express";
 import * as controller from "../controllers/categoryController";
 import authenticate from "../middlewares/authenticate";
+import z from "zod";
+import { validate } from "../middlewares/validation";
+import { Types } from "mongoose";
 
 const router = express.Router();
 
-router.post("/add", authenticate, controller.addCategory);
+const addSchema = z.object({
+  body: z.object({ name: z.string() }),
+});
+
+const updateSchema = z.object({
+  body: z.object({
+    categoryId: z.string().refine((id) => Types.ObjectId.isValid(id), {
+      message: "invalid ObjectId",
+    }),
+    data: z.object({ name: z.string() }),
+  }),
+});
+
+router.post("/add", authenticate, validate(addSchema), controller.addCategory);
 router.get("/getAllMy", authenticate, controller.getMyCategorys);
-router.put("/updateMy", authenticate, controller.updateMyCategory);
+router.put(
+  "/updateMy",
+  authenticate,
+  validate(updateSchema),
+  controller.updateMyCategory
+);
 router.delete("/deleteMy/:id", authenticate, controller.deleteMyCategoryById);
 
 export default router;
