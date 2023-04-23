3e92a48d485231db10159e7b4a106426d4623cf9
Lorp91 <92474455+Lorp91@users.noreply.github.com>
2023-04-23 21:05:37 +0200

    tag validation added

--- src/routes/tagRoute.ts
+++ src/routes/tagRoute.ts
@@ -1,12 +1,31 @@
 import express from "express";
 import * as controller from "../controllers/tagController";
 import authenticate from "../middlewares/authenticate";
+import z from "zod";
+import { validate } from "../middlewares/validation";
+import { Types } from "mongoose";
 
 const router = express.Router();
 
-router.post("/add", authenticate, controller.addTag);
+const addSchema = z.object({ body: z.object({ name: z.string() }) });
+
+const updateSchema = z.object({
+  body: z.object({
+    tagId: z.string().refine((id) => Types.ObjectId.isValid(id), {
+      message: "invalid ObjectId",
+    }),
+    data: z.object({ name: z.string().optional() }),
+  }),
+});
+
+router.post("/add", authenticate, validate(addSchema), controller.addTag);
 router.get("/getAllMy", authenticate, controller.getMyTags);
-router.put("/updateMy", authenticate, controller.updateMyTag);
+router.put(
+  "/updateMy",
+  authenticate,
+  validate(updateSchema),
+  controller.updateMyTag
+);
 router.delete("/deleteMy/:id", authenticate, controller.deleteMyTagById);
 
 export default router;
