17752a4d0ceabf5b61d569040aef0e38d4db45e1
SebastianWolter1 <seb@techtakel.berlin>
2023-04-25 14:47:26 +0200

    Models updated

--- src/models/Budget.ts
+++ src/models/Budget.ts
@@ -1,38 +1,48 @@
-import mongoose, {Model, Schema, Types} from "mongoose";
+import mongoose, { Model, Schema, Types } from "mongoose";
 
 export interface IBudget {
-    user: Types.ObjectId;
-    product: string;
-    amount: number;  
-    amountleft: number;  
+  user: Types.ObjectId;
+  item: string;
+  amount: number;
+  amountspent: number;
+  amountleft: number;
 }
 
 type BudgetModel = Model<IBudget>;
 
 export const BudgetSchema = new Schema<IBudget, BudgetModel>({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    product: {
-        type: String,
-        required: true,
-        minlength: 1,
-    },
-    amount: {
-        type: Number,
-        required: true,
-        min: 0
-    },
-    amountleft: {
-        type: Number,
-        required: true,
-        min: this.amount,
-        max: 0
-    }
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  item: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  amount: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  amountspent: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  amountleft: {
+    type: Number,
+    required: true,
+    max: 0,
+  },
 });
 
-export const Budget = mongoose.model<IBudget, BudgetModel>("Budget", BudgetSchema);
+BudgetSchema.pre<IBudget>("save", function () {
+  this.amountleft = this.amount - this.amountspent;
+});
 
-
+export const Budget = mongoose.model<IBudget, BudgetModel>(
+  "Budget",
+  BudgetSchema
+);
--- src/models/Wishes.ts
+++ src/models/Wishes.ts
@@ -1,37 +1,42 @@
-import mongoose, {Model, Schema, Types} from "mongoose";
+import mongoose, { Model, Schema, Types } from "mongoose";
 
 export interface IWish {
-    user: Types.ObjectId;
-    name: string;
-    price: number;
-    saved: number;
-}   
+  user: Types.ObjectId;
+  name: string;
+  price: number;
+  moneysaved: number;
+  moneyleft: number;
+}
 
 type WishModel = Model<IWish>;
 
 export const WishSchema = new Schema<IWish, WishModel>({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    name: {
-        type: String,
-        required: true,
-        minlength: 1,
-    },
-    price: {
-        type: Number,
-        required: true,
-        min: 1
-    },
-    saved: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: this.price
-    }
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  moneyleft: {
+    type: Number,
+    required: true,
+  },
+  moneysaved: {
+    type: Number,
+    required: true,
+  },
+});
+
+WishSchema.pre<IWish>("save", function () {
+  this.moneyleft = this.price - this.moneysaved;
 });
 
 export const Wish = mongoose.model<IWish, WishModel>("Wish", WishSchema);
-
