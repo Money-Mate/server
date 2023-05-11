254d9c6f2092761b6d0c269ae09033945a7aefc9
Lorp91 <92474455+Lorp91@users.noreply.github.com>
2023-05-11 09:54:07 +0200

    wish logic added

---
+++ src/controllers/wishController.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from "express";
+import { IWish, Wish } from "../models/Wish";
+
+export const addWish = async (req: Request, res: Response) => {
+  try {
+    const newWish: Partial<IWish> = {};
+
+    // required fields
+    newWish.name = req.body.name;
+    newWish.price = req.body.price;
+
+    const data = await new Wish(newWish).save();
+    res.json({ msg: "Wish created", data });
+  } catch (err) {
+    console.log(err);
+    res.json({ msg: "server error" });
+  }
+};
+
+export const getMyWishes = async (req: Request, res: Response) => {
+  try {
+    const data = await Wish.find({ user: res.locals.user._id }).select({
+      user: false,
+      __v: false,
+    });
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.json({ msg: "server error" });
+  }
+};
+
+export const updateMyWish = async (req: Request, res: Response) => {
+  try {
+    const data = await Wish.updateOne(
+      { user: res.locals.user._id, _id: req.body.wishId },
+      { ...req.body.data }
+    );
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.json({ msg: "server error" });
+  }
+};
+
+export const deleteMyWish = async (req: Request, res: Response) => {
+  try {
+    const data = await Wish.deleteOne({
+      user: res.locals.user._id,
+      _id: req.params.id,
+    });
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.json({ msg: "server error" });
+  }
+};
--- src/models/Wish.ts
+++ src/models/Wish.ts
@@ -4,8 +4,6 @@ export interface IWish {
   user: Types.ObjectId;
   name: string;
   price: number;
-  moneysaved: number;
-  moneyleft: number;
 }
 
 type WishModel = Model<IWish>;
@@ -20,23 +18,12 @@ export const WishSchema = new Schema<IWish, WishModel>({
     type: String,
     required: true,
     minlength: 1,
+    unique: true,
   },
   price: {
     type: Number,
     required: true,
   },
-  moneyleft: {
-    type: Number,
-    required: true,
-  },
-  moneysaved: {
-    type: Number,
-    required: true,
-  },
-});
-
-WishSchema.pre<IWish>("save", function () {
-  this.moneyleft = this.price - this.moneysaved;
 });
 
 export const Wish = mongoose.model<IWish, WishModel>("Wish", WishSchema);
